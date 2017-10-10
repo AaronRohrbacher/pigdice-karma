7ee05d7278e2aeb7d00ae6ab04ff190ad1033ab6
Guest User <Guest@Epicodus-6D.local>
2017-10-10 10:54:30 -0700

    fix tests

--- spec/pigdice-spec.js
+++ spec/pigdice-spec.js
@@ -3,10 +3,10 @@ var Player = require('./../js/pigdice.js').pigdiceModule;
 describe('Player', function () {
   it('should return a name entered by user', function() {
     var playa = new Player("Javi")
-    expect(playa.name.toEqual("Javi")
+    expect(playa.name).toEqual("Javi");
   });
-  // it('should return a number when user rolls', function() {
-  //   var playa = new Player("AAron")
-  //   expect(playa.roll()).toEqual(jasmine.any(Number));
-  // });
+  it('should return a number when user rolls', function() {
+    var playa = new Player("AAron")
+    expect(playa.roll()).toEqual(jasmine.any(Number));
+  });
 });
